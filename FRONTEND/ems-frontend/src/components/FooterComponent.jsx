c4f0fbce399696b7cb30a5d79fd727bb25a03c8f
RISHABHXVAZ <srivastavarishabh799@gmail.com>
2025-10-22 13:51:41 +0530

    Added Header and Footer Components

---
+++ FRONTEND/ems-frontend/src/components/FooterComponent.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const FooterComponent = () => {
+  return (
+    <div>
+        <footer className='footer'>
+           <span>All rights reserved 2025 | <a href='http://linkedin.com/in/rishabh-srivastava-45a739314'>Rishabh Srivastava</a></span>
+        </footer>
+    </div>
+  )
+}
+
+export default FooterComponent
---
+++ FRONTEND/ems-frontend/src/components/HeaderComponent.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const HeaderComponent = () => {
+  return (
+    <div>
+        <header>
+            <nav className='navbar navbar-dark bg-dark'>
+                <a className="navbar-brand" href="http://github.com/RISHABHXVAZ/Employee-Management-System.git">Employee Management System</a>
+            </nav>
+        </header>
+    </div>
+  )
+}
+
+export default HeaderComponent
