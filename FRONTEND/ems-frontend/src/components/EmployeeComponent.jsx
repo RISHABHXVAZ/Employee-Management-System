9a94f1145cab008ff7714a7aaa78927fe5d00b1b
RISHABHXVAZ <srivastavarishabh799@gmail.com>
2025-10-25 12:36:19 +0530

    Validated Form

--- FRONTEND/ems-frontend/src/components/EmployeeComponent.jsx
+++ FRONTEND/ems-frontend/src/components/EmployeeComponent.jsx
@@ -9,17 +9,54 @@ const EmployeeComponent = () => {
   const [lastName, setLastName] = useState('')
   const [email, setEmail] = useState('')
 
+  const [errors, setErrors] = useState({
+    firstName: '',
+    lastName: '',
+    email: ''
+  })
+
   const navigator = useNavigate()
 
-  function saveEmployee(e){
-    e.preventDefault();
-    const employee = {firstName, lastName, email}
-    console.log(employee)
+    function saveEmployee(e){
+        e.preventDefault();
+        if(validateForm()){
+            const employee = {firstName, lastName, email}
+            console.log(employee)
 
-    createEmployee(employee).then((response) => {
-        console.log(employee.data)
-        navigator('/employees')
-    })
+            createEmployee(employee).then((response) => {
+                console.log(employee.data)
+                navigator('/employees')
+            })
+        }
+    }
+
+  function validateForm(){
+    let valid = true
+    const errorsCopy = {...errors}
+    if(firstName.trim()){
+        errorsCopy.firstName = ''
+    }else{
+        errorsCopy.firstName = 'First Name is Required!'
+        valid = false;
+    }
+
+     if(lastName.trim()){
+        errorsCopy.lastName = ''
+    }else{
+        errorsCopy.lastName = 'Last Name is Required!'
+        valid = false;
+    }
+
+    if(email.trim()){
+        errorsCopy.email = ''
+    }else{
+        errorsCopy.email = 'Email is Required!'
+        valid = false;
+    }
+
+    setErrors(errorsCopy)
+    return valid;
+
   }
   return (
     <div className='container'>
@@ -41,8 +78,12 @@ const EmployeeComponent = () => {
                             placeholder='Enter Employee First Name'
                             name='firstName'
                             value={firstName}
-                            className='form-control'
-                            onChange={(e) => setFirstName(e.target.value)}></input>
+                            className= {`form-control ${ errors.firstName ? 'is-invalid': ''}`}
+                            onChange={(e) => setFirstName(e.target.value)}>
+                           </input>
+                           {
+                            errors.firstName && <div className='invalid-feedback'>{errors.firstName}</div>
+                           }
                         </div>
                         <div className='form-group mb-2'>
                             <label className='form-label'>Last Name: </label>
@@ -51,8 +92,11 @@ const EmployeeComponent = () => {
                             placeholder='Enter Employee Last Name'
                             name='lastName'
                             value={lastName}
-                            className='form-control'
+                            className={`form-control ${ errors.lastName ? 'is-invalid': ''}`}
                             onChange={(e) => setLastName(e.target.value)}></input>
+                            {
+                            errors.lastName && <div className='invalid-feedback'>{errors.lastName}</div>
+                           }
                         </div>
                         <div className='form-group mb-2'>
                             <label className='form-label'>Email: </label>
@@ -61,8 +105,11 @@ const EmployeeComponent = () => {
                             placeholder='Enter Employee Email'
                             name='Email'
                             value={email}
-                            className='form-control'
+                            className={`form-control ${ errors.email ? 'is-invalid': ''}`}
                             onChange={(e) => setEmail(e.target.value)}></input>
+                            {
+                            errors.email && <div className='invalid-feedback'>{errors.email}</div>
+                           }
                         </div>
                         <button className='btn btn-success' onClick={saveEmployee}>Submit</button>
                     </form>
